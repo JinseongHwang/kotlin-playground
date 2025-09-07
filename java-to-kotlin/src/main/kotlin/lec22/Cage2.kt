a56366fee6f17ecd5f8f89a957b63651cc2428ac
JinseongHwang <jinseong.dev@gmail.com>
2025-09-07 19:45:03 +0900

    feat: moveFrom 메서드 파라미터를 무공변에서 공변으로 만듦 (변성;variance을 주다)

--- java-to-kotlin/src/main/kotlin/lec22/Cage2.kt
+++ java-to-kotlin/src/main/kotlin/lec22/Cage2.kt
@@ -9,8 +9,12 @@ class Cage2<T> {
         this.animals.add(animal)
     }
 
-    fun moveFrom(cage: Cage2<T>) {
-        this.animals.addAll(cage.animals)
+    fun moveFrom(otherCage: Cage2<out T>) {
+        this.animals.addAll(otherCage.animals)
+
+        // out 을 붙이면 파라미터가 데이터 생산자 역할만 할 수 있다.
+//        otherCage.getFirst() // out을 사용하면 조회는 가능
+//        otherCage.put(GoldFish("금붕어")) // out을 사용하면 저장 불가능!
     }
 }
 
@@ -19,5 +23,7 @@ fun main() {
     goldFishCage.put(GoldFish("금붕어"))
 
     val cage = Cage2<Fish>()
-//    cage.moveFrom(goldFishCage) // Error: Type mismatch
+    cage.moveFrom(goldFishCage)
+
+    val fish: Fish = cage.getFirst()
 }
