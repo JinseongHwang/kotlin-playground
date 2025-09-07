c2d47268141e9bca4431291ba177fedebf4622bb
JinseongHwang <jinseong.dev@gmail.com>
2025-09-07 19:58:30 +0900

    feat: moveTo 메서드 파라미터를 무공변에서 반공변으로 만듦(contra-variant)

--- java-to-kotlin/src/main/kotlin/lec22/Cage2.kt
+++ java-to-kotlin/src/main/kotlin/lec22/Cage2.kt
@@ -12,18 +12,24 @@ class Cage2<T> {
     fun moveFrom(otherCage: Cage2<out T>) {
         this.animals.addAll(otherCage.animals)
 
-        // out 을 붙이면 파라미터가 데이터 생산자 역할만 할 수 있다.
+        // out 을 붙이면 파라미터가 데이터 생산자 역할만 할 수 있다. 공변화 한다.
 //        otherCage.getFirst() // out을 사용하면 조회는 가능
 //        otherCage.put(GoldFish("금붕어")) // out을 사용하면 저장 불가능!
+    }
+
+    fun moveTo(otherCage: Cage2<in T>) {
+        otherCage.animals.addAll(this.animals)
+//        val fish: Fish = otherCage.getFirst()
+
+        // in이 붙은 otherCage는 데이터를 받을 수만 있다! 반공변화 한다.
+        // otherCage는 데이터 소비자이다.
     }
 }
 
 fun main() {
+    val fishCage = Cage2<Fish>()
+
     val goldFishCage = Cage2<GoldFish>()
     goldFishCage.put(GoldFish("금붕어"))
-
-    val cage = Cage2<Fish>()
-    cage.moveFrom(goldFishCage)
-
-    val fish: Fish = cage.getFirst()
+    goldFishCage.moveTo(fishCage)
 }
